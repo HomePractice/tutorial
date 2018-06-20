50c27b1c825b1ba32341fc26e5157b43aa41f57d
shweta <shweta.kc016@gmail.com>
2018-06-20 17:12:54 +0545

    scape sequence

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            //  string Name = "\"shweta\"";
+            //string Name = "one\ntwo\nthree";
+            string Name = @"c:\\shweta\\dotnet\\traning\\csharp";
+                Console.WriteLine(Name);
+            Console.ReadKey();
+              
+        }
+    }
+}
