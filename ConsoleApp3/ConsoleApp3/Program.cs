45f0d70066635a205a1b721e52bd0770d2068413
shweta <shweta.kc016@gmail.com>
2018-06-20 14:08:51 +0545

    min value and max value

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+          Console.WriteLine("  Min = {0} " , int.MinValue);
+            Console.WriteLine("Max ={0} ", int.MaxValue);
+            Console.ReadKey();
+        }
+    }
+}
