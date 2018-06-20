3930ecd09d830e818a74bb59dce546c21b80aff4
shweta <shweta.kc016@gmail.com>
2018-06-20 13:46:39 +0545

    first and last name has been entered

---
+++ class two/class two/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_two
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter your  first name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("enter your  last name");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("hello {0} , {1}", FirstName ,LastName);
+           // Console.WriteLine("hello " + UserName);
+            Console.ReadKey();
+        }
+    }
+}
